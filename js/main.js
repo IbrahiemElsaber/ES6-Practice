4ea05df68250ebc1bf6c5cd52e5d5b0cc41a4404
IbrahiemElsaber <ibrahimelsaber8@gmail.com>
2020-05-29 13:28:16 +0200

    Creating js and css files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//we can't use the same value of this in two functions
+// function Cars(){
+//     this.carCounter = 0;
+//     setInterval(function(){
+//         this.carCounter++;
+//         console.log(this.carCounter);
+//     }, 1000)
+// }
+
+// let newCar = new Cars();
+
+//fisrt solution to the pervious problem [ regular function]
+
+// function Cars(){
+//     let that = this;
+//     this.carCounter = 0;
+//     setInterval(function(){
+//         that.carCounter++;
+//         console.log(that.carCounter);
+//     }, 1000);
+// }
+// let newCar = new Cars();
+
+//second solution using [arrow function]
+
+function Cars(){
+    this.carCounter = 0;
+    setInterval(()=>{
+        this.carCounter++;
+        console.log(this.carCounter);
+    }, 1000)
+}
+let newCar = new Cars();
