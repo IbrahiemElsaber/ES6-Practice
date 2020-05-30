c67a2d24036b0c196aed0266323b189c45db1e10
IbrahiemElsaber <ibrahimelsaber8@gmail.com>
2020-05-30 08:38:32 +0200

    Practicing some spread operator examples

--- js/main.js
+++ js/main.js
@@ -41,3 +41,17 @@
 //     numbers = oldNumbers.concat(newNumbers);
 
 // console.log(...oldNumbers.concat(newNumbers).concat(otherNumbers));
+
+
+//clone array to another without using spread operator
+// let arrayOne = [1, 2, 3];
+    // arrayTwo = arrayOne;
+
+// arrayOne.push('t');
+// console.log(arrayOne);
+// console.log(arrayTwo);
+//using spread operator
+// let arrayTwo = [...arrayOne];
+// arrayTwo.push('S');
+// console.log(arrayOne);
+// console.log(arrayTwo);
